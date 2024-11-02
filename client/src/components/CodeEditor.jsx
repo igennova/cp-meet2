e51a066d0c369b906729a46596de837319a0b452
devSuryansh <suryanshg2050@gmail.com>
2024-11-02 18:09:05 +0530

    Inputs the user details before question

--- client/src/components/CodeEditor.jsx
+++ client/src/components/CodeEditor.jsx
@@ -3,7 +3,7 @@ import { Box, HStack } from "@chakra-ui/react";
 import { Editor } from "@monaco-editor/react";
 import LanguageSelector from "./LanguageSelector";
 import { CODE_SNIPPETS } from "../constants";
-import Output from "./Output";
+// import Output from "./Output";
 import RandomQuestion from "./questions/question";
 
 const CodeEditor = () => {
