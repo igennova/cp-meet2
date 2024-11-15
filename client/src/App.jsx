6e2b7d8ade906f4e3c508a64585fd92a79dfbedf
devSuryansh <suryanshg2050@gmail.com>
2024-11-15 12:44:03 +0530

    Adds routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { io } from "socket.io-client";
-import { Footer, CodeEditor } from "@/components";
+import { Footer, CodeEditor, Navbar } from "@/components";
 import { Box } from "@chakra-ui/react";
-import { BrowserRouter, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Button, Input, HyperText, GradualSpacing } from "@/components/ui";
 const socket = io("http://localhost:5000");
 import { toast, ToastContainer } from "react-toastify";
+import { Home, Rules } from "@/pages";
 
 const App = () => {
   const [roomId, setRoomId] = useState("");
@@ -106,88 +107,41 @@ const App = () => {
 
   return (
     <BrowserRouter>
-      <div>
-        <nav className="w-full flex justify-between items-center sm:px-8 px-4 py-4 border-b border-b-gray-500">
-          <Link to="/">
-            <div className="flex items-center space-x-4">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className="h-6 mr-3 text-white sm:h-9"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M17.25 6.75 22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3-4.5 16.5"
-                />
-              </svg>
-              <div className="text-white self-center text-xl font-semibold whitespace-nowrap">
-                CP Buddy
-              </div>
-            </div>
-          </Link>
-          {isMatched && !timeup ? (
-            <div className="text-2xl font-medium text-white">
-              {formatTime(time)}
-            </div>
-          ) : null}
-        </nav>
+      <Navbar
+        isMatched={isMatched}
+        timeup={timeup}
+        formatTime={formatTime}
+        time={time}
+      />
 
-        {timeup ? (
-          <div className="text-center text-4xl text-white py-8">
-            Time's Up! The game is over.
-          </div>
-        ) : isMatched ? (
-          <Box minH="100vh" bg="#0f0a19" color="gray.500" px={6} py={8}>
-            <CodeEditor socket={socket} roomId={roomId} userName={userName} />
-          </Box>
-        ) : (
-          <div className="flex items-center justify-center min-h-screen">
-            <div className="w-full max-w-md p-4 space-y-4">
-              <HyperText
-                className="text-4xl font-bold text-white"
-                text="1V1 DSA BATTLE"
-              />
-              <Input
-                type="text"
-                placeholder="Enter Your Name"
-                value={userName}
-                onChange={(e) => setUserName(e.target.value)}
-                className="w-full"
-              />
-              <Input
-                type="text"
-                placeholder="Enter Room ID"
-                value={roomId}
-                onChange={(e) => setRoomId(e.target.value)}
-                className="w-full"
-              />
-              <Button
-                onClick={createRoom}
-                disabled={isRoomCreated}
-                className="w-full font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md"
-              >
-                Create Room
-              </Button>
-              <Button
-                onClick={joinRoom}
-                disabled={isRoomJoined || isRoomCreated}
-                className="w-full font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md"
-              >
-                Join Room
-              </Button>
-              <GradualSpacing
-                className="font-display text-center text-4xl font-bold -tracking-widest text-white"
-                text={gameMessage}
-              />
-            </div>
-          </div>
-        )}
-      </div>
-      {/* <Footer /> */}
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Home
+              timeup={timeup}
+              isMatched={isMatched}
+              userName={userName}
+              setUserName={setUserName}
+              roomId={roomId}
+              setRoomId={setRoomId}
+              isRoomCreated={isRoomCreated}
+              isRoomJoined={isRoomJoined}
+              gameMessage={gameMessage}
+              createRoom={createRoom}
+              joinRoom={joinRoom}
+              socket={socket}
+              CodeEditor={CodeEditor}
+              HyperText={HyperText}
+              Input={Input}
+              Button={Button}
+              GradualSpacing={GradualSpacing}
+            />
+          }
+        />
+
+        <Route path="/rules" element={<Rules />} />
+      </Routes>
     </BrowserRouter>
   );
 };
