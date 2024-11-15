7c6562c14b00aaf5de5925eaab9881792eea0a8d
devSuryansh <suryanshg2050@gmail.com>
2024-11-15 16:18:31 +0530

    Modifies the Language Selector

--- client/src/components/LanguageSelector.jsx
+++ client/src/components/LanguageSelector.jsx
@@ -6,6 +6,7 @@ import {
   MenuItem,
   MenuList,
   Text,
+  Flex
 } from "@chakra-ui/react";
 import { LANGUAGE_VERSIONS } from "@/constants";
 
@@ -15,11 +16,26 @@ const ACTIVE_COLOR = "blue.400";
 const LanguageSelector = ({ language, onSelect }) => {
   return (
     <Box ml={2} mb={4}>
-      <Text mb={2} fontSize="lg">
+      <Text mb={2} fontSize="lg" color="whiteAlpha.700">
         Language:
       </Text>
       <Menu isLazy>
-        <MenuButton as={Button}>{language}</MenuButton>
+        <MenuButton as={Button}>
+          <Flex alignItems="center">
+            {language}
+            <Box as="span" ml={2} display="inline-block">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 512 512"
+                width="16"
+                height="16"
+                fill="white"
+              >
+                <path d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z" />
+              </svg>
+            </Box>
+          </Flex>
+        </MenuButton>
         <MenuList bg="#110c1b">
           {languages.map(([lang, version]) => (
             <MenuItem
