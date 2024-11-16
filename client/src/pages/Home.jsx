490419343ec32bfcfe730d14cc375f0c248b8947
devSuryansh <suryanshg2050@gmail.com>
2024-11-16 12:22:42 +0530

    Makes the site inaccessbile for smaller screens

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,4 +1,11 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
 import { Button, Input, HyperText, GradualSpacing } from "@/components/ui";
 import { Box } from "@chakra-ui/react";
 import { CodeEditor } from "@/components";
@@ -17,9 +24,61 @@ const Home = ({
   joinRoom,
   socket,
 }) => {
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
+
+  useEffect(() => {
+    const checkScreenSize = () => {
+      setIsSmallScreen(window.innerWidth < 768);
+    };
+
+    checkScreenSize();
+
+    window.addEventListener("resize", checkScreenSize);
+
+    return () => {
+      window.removeEventListener("resize", checkScreenSize);
+    };
+  }, []);
+
   return (
     <>
-      {timeup ? (
+      {isSmallScreen ? (
+        <Dialog open>
+          <DialogContent
+            className="bg-black text-gray-200"
+            style={{
+              maxWidth: `calc(100% - 50px)`,
+              height: "40%",
+              margin: "auto",
+              padding: "20px",
+              border: "1px solid gray",
+              borderRadius: "10px",
+              boxShadow: "0 4px 10px rgba(0, 0, 0, 0.5)",
+              overflowY: "auto",
+            }}
+          >
+            <DialogHeader>
+              <DialogTitle
+                className="text-white text-xl text-center"
+                style={{ marginBottom: "15px", fontWeight: "bold" }}
+              >
+                Access Restricted
+              </DialogTitle>
+              <DialogDescription
+                className="text-gray-400 text-base text-center"
+                style={{ lineHeight: "1.6", fontSize: "1.2rem" }}
+              >
+                You can't access this website on a smaller screen. Please open
+                this website on a laptop or desktop screen.
+                <br />
+                <br />
+                We're working on making this site responsive and accessible on
+                smartphones as well. Stay tuned!
+              </DialogDescription>
+            </DialogHeader>
+          </DialogContent>
+        </Dialog>
+      ) : timeup ? (
         <div className="text-center text-4xl text-white py-8">
           Time's Up! The game is over.
         </div>
